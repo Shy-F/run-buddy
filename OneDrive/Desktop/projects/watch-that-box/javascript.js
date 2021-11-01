132aacd59fdf473a878cb2360a60fefe5496ff02
Shy <fleetwoodshybreasia@gmail.com>
2021-11-01 14:16:50 -0400

    html outline and gitignore

---
+++ OneDrive/Desktop/projects/watch-that-box/javascript.js
@@ -0,0 +1,15 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "250px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box")
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height = "150px";
+});
